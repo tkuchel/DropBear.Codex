002ce06d1fe82fc74753724b9026d9da7516cccf
DropBear <tkuchel@users.noreply.github.com>
2024-09-10 11:28:22 +1000

    fix: snackbar service was not passing a default title to the options. add: added an additional overload for the snackbar service to allow for passing a title and message.

--- DropBear.Codex.Blazor/Interfaces/ISnackbarNotificationService.cs
+++ DropBear.Codex.Blazor/Interfaces/ISnackbarNotificationService.cs
@@ -37,6 +37,21 @@ public interface ISnackbarNotificationService
         bool isDismissible = true, string actionText = "Dismiss", Func<Task>? onAction = null);
 
     /// <summary>
+    ///     Shows a snackbar notification with the specified message and options.
+    /// </summary>
+    /// <param name="title">The title to display.</param>
+    /// <param name="message">The message to display.</param>
+    /// <param name="type">The type of the snackbar.</param>
+    /// <param name="duration">The duration to display the snackbar in milliseconds.</param>
+    /// <param name="isDismissible">Indicates whether the snackbar notification is dismissible.</param>
+    /// <param name="actionText">The text of the action button on the snackbar notification.</param>
+    /// <param name="onAction">The action to perform when the action button is clicked.</param>
+    /// <returns>A task representing the result of the operation.</returns>
+    Task<Result> ShowAsync(string title, string message, SnackbarType type = SnackbarType.Information,
+        int duration = 5000,
+        bool isDismissible = true, string actionText = "Dismiss", Func<Task>? onAction = null);
+
+    /// <summary>
     ///     Hides all currently visible snackbar notifications.
     /// </summary>
     /// <returns>A task that represents the completion of the operation.</returns>
