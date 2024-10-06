bf2cd7233be29ac3c4b4d70b44fe56f718bd45d4
DropBear <tkuchel@users.noreply.github.com>
2024-10-06 14:28:43 +1100

    fix: still had the default id for the openfiledialog jsinterop

--- DropBear.Codex.Blazor/Components/Files/DropBearFileUploader.razor.cs
+++ DropBear.Codex.Blazor/Components/Files/DropBearFileUploader.razor.cs
@@ -198,6 +198,6 @@ public partial class DropBearFileUploader : DropBearComponentBase, IDisposable
     /// </summary>
     private async Task OpenFileDialog()
     {
-        await JsRuntime.InvokeVoidAsync("clickElement", _fileInputRef);
+        await JsRuntime.InvokeVoidAsync("fileInput", _fileInputRef);
     }
 }
