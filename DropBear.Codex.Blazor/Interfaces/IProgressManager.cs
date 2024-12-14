fe3839fd1c36896d7d6de04db0b97b8857b69e82
DropBear <tkuchel@users.noreply.github.com>
2024-12-14 16:38:06 +1100

    added IsDisposed property to IProgressManager.cs

--- DropBear.Codex.Blazor/Interfaces/IProgressManager.cs
+++ DropBear.Codex.Blazor/Interfaces/IProgressManager.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿#region
+
 using DropBear.Codex.Blazor.Enums;
 using DropBear.Codex.Blazor.Models;
 
-namespace DropBear.Codex.Blazor.Interfaces
+#endregion
+
+namespace DropBear.Codex.Blazor.Interfaces;
+
+public interface IProgressManager : IDisposable
 {
-    public interface IProgressManager : IDisposable
-    {
-        string Message { get; }
-        double Progress { get; }
-        bool IsIndeterminate { get; }
-        IReadOnlyList<ProgressStepConfig>? Steps { get; }
-        IReadOnlyList<StepState> CurrentStepStates { get; }
+    string Message { get; }
+    double Progress { get; }
+    bool IsIndeterminate { get; }
+    IReadOnlyList<ProgressStepConfig>? Steps { get; }
+    IReadOnlyList<StepState> CurrentStepStates { get; }
 
-        CancellationToken CancellationToken { get; }
+    CancellationToken CancellationToken { get; }
 
-        event Action? StateChanged;
+    bool IsDisposed { get; }
 
-        void StartIndeterminate(string message);
-        void StartTask(string message);
-        void StartSteps(List<ProgressStepConfig> steps);
-        Task UpdateProgressAsync(string taskId, double progress, StepStatus status, string? message = null);
-        void Complete();
-    }
+    event Action? StateChanged;
+
+    void StartIndeterminate(string message);
+    void StartTask(string message);
+    void StartSteps(List<ProgressStepConfig> steps);
+    Task UpdateProgressAsync(string taskId, double progress, StepStatus status, string? message = null);
+    void Complete();
 }
